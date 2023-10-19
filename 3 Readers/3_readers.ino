6c6cfca608c1efcd083b6725ba53f9cda3f8b72a
Ahmed Sherif <ahmed.sherif.a.2008@gmail.com>
2023-10-19 17:34:50 +0300

    Optimized the code to be more clean and to follow the SOLID principles, and fast reading.

--- 3 Readers/3_readers.ino
+++ 3 Readers/3_readers.ino
@@ -2,153 +2,94 @@
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
-#define PN532_SCK  (13) //Green
-#define PN532_MOSI (11) //Orange
-#define PN532_MISO (12) //Yellow
+// Define pin constants for PN532
+#define PN532_SCK   13 // Green
+#define PN532_MOSI  11 // Orange
+#define PN532_MISO  12 // Yellow
+#define PN532_SS    10 // Red
+#define PN532_SS_2   9 // Brown
+#define PN532_SS_3   8 // Green
 
-#define PN532_SS   (10) //Red
-#define PN532_SS_2  (9) //Brown
-#define PN532_SS_3  (8) //Brown
+// Define LED pins
+#define GREEN_LED 2
+#define RED_LED   3
 
-// ADD Scanner 1 and Scanner 2
-Adafruit_PN532 ScannerOne(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-Adafruit_PN532 ScannerTwo(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS_2);
-Adafruit_PN532 ScannerThree(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS_3);
+// Create an array of scanners
+Adafruit_PN532 scanners[] = {
+  Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS),
+  Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS_2),
+  Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS_3)
+};
 
-void setup(void) {
+
+
+void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  // Setup and Check Scanner 1
-  ScannerOne.begin();
-  uint32_t versiondata = ScannerOne.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.println("First PN532 board not found");
-    while (1);
-  }
-  ScannerOne.SAMConfig();
+  while (!Serial) delay(10); // Wait for serial to initialize
 
-  // Setup and Check Scanner 2
-  ScannerTwo.begin();
-  uint32_t versiondata2 = ScannerTwo.getFirmwareVersion();
-  if (! versiondata2) {
-    Serial.println("Second PN532 board not found");
-    while (1); 
-  }
-  ScannerTwo.SAMConfig();
+  // Initialize LED pins
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
 
-  // Setup and Check Scanner 3
-  ScannerThree.begin();
-  uint32_t versiondata3 = ScannerThree.getFirmwareVersion();
-  if (! versiondata3) {
-    Serial.println("Third PN532 board not found");
-    while (1); 
+  // Setup and check each scanner
+  for (int i = 0; i < 3; i++) {
+    scanners[i].begin();
+    uint32_t versiondata = scanners[i].getFirmwareVersion();
+    if (!versiondata) {
+      Serial.print("Scanner ");
+      Serial.print(i + 1);
+      Serial.println(": Not found");
+      while (1); // Halt program
+    }
+    scanners[i].SAMConfig();
   }
-  ScannerThree.SAMConfig();
-  
+
   Serial.println("Waiting for Card ...");
 }
 
-void loop(void) {
-  uint8_t uidOne[] = { 0, 0, 0, 0, 0, 0, 0 };
-  uint8_t uidTwo[] = { 0, 0, 0, 0, 0, 0, 0 };  
-  uint8_t uidThree[] = { 0, 0, 0, 0, 0, 0, 0 };  
-  uint8_t uidLengthOne;                        
-  uint8_t uidLengthTwo; 
-  uint8_t uidLengthThree; 
+void handleCard(int scannerIndex, uint8_t uid[], uint8_t uidLength) {
+  Serial.print("Scanner ");
+  Serial.print(scannerIndex + 1);
+  Serial.print(": UID: ");
+  scanners[scannerIndex].PrintHex(uid, uidLength);
 
+  bool isBlank = (uid[0] == 0x00 && uid[1] == 0x00 && uid[2] == 0x00 && uid[3] == 0x00);
 
-  // Checking Scanner 1 for Card
-  if (ScannerOne.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-      uint8_t success = ScannerOne.readPassiveTargetID(PN532_MIFARE_ISO14443A, uidOne, &uidLengthOne);
-  }
+  bool Cards[] = {
+   ((uid[0] == 0x53 && uid[1] == 0x05 && uid[2] == 0x4F && uid[3] == 0xFA) ||
+                 (uid[0] == 0x53 && uid[1] == 0x72 && uid[2] == 0xFA && uid[3] == 0xAB)),
+                 
+   ((uid[0] == 0x53 && uid[1] == 0x05 && uid[2] == 0x4F && uid[3] == 0xFA) ||
+                 (uid[0] == 0x53 && uid[1] == 0x72 && uid[2] == 0xFA && uid[3] == 0xAB)),
+                 
+   ((uid[0] == 0x53 && uid[1] == 0x05 && uid[2] == 0x4F && uid[3] == 0xFA) ||
+                 (uid[0] == 0x53 && uid[1] == 0x72 && uid[2] == 0xFA && uid[3] == 0xAB)),
+  };
 
-  // Checking Scanner 2 for Card
-  if (ScannerTwo.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-       uint8_t success2 = ScannerTwo.readPassiveTargetID(PN532_MIFARE_ISO14443A, uidTwo, &uidLengthTwo);
-  }
-
-  // Checking Scanner 3 for Card
-  if (ScannerThree.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-       uint8_t success3 = ScannerThree.readPassiveTargetID(PN532_MIFARE_ISO14443A, uidThree, &uidLengthThree);
-  }
-
-
-  if (!ScannerOne.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerTwo.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerThree.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-    // Scanner 1 and Scanner 2 NOT found
-    //delay(1000);
-    return;
-  } else if (ScannerOne.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerTwo.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerThree.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-    // Scanner 1 found
-    Serial.print("Scanner 1: UID: ");
-    ScannerOne.PrintHex(uidOne, uidLengthOne);
-    if((uidOne[0] == 0x53 && uidOne[1] == 0x05 && uidOne[2] == 0x4F && uidOne[3] == 0xFA) || (uidOne[0] == 0x53 && uidOne[1] == 0x72 && uidOne[2] == 0xFA && uidOne[3] == 0xAB)) {
-       // Scanner 1: UID: 0x53 0x72 0xFA 0xAB
-
-        Serial.println("Yess, you got it right this is an even number");
-        digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(2000);                       // wait for a second
-        digitalWrite(2, LOW);
- } else {
-          Serial.println("Oops, this is not an even number try again");
-          digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-          delay(2000);                       // wait for a second
-          digitalWrite(3, LOW);
-  }
-   // Serial.print("Scanner 2: UID: Not found");
-  } else if (!ScannerOne.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && ScannerTwo.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerThree.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-    // Scanner 2 found
-    
-  //  Serial.print("Scanner 1: UID: Not found");
-    Serial.print("Scanner 2: UID: ");
-    ScannerTwo.PrintHex(uidTwo, uidLengthTwo);
-    if((uidTwo[0] == 0x33 && uidTwo[1] == 0x26 && uidTwo[2] == 0x84 && uidTwo[3] == 0xFA) || (uidTwo[0] == 0xA3 && uidTwo[1] == 0xA5 && uidTwo[2] == 0xE4 && uidTwo[3] == 0xAB)) {
-       // Scanner 2: UID: 0xA3 0xA5 0xE4 0xAB
-
-        Serial.println("Yess, you got it right this is an odd number");
-        digitalWrite(2, HIGH);  
-        delay(2000);
-        digitalWrite(2, LOW); 
-        
- } else {
-          Serial.println("Oops, this is not an odd number try again");
-          digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-          delay(2000);                       // wait for a second
-          digitalWrite(3, LOW);
-  }
-
-  } else if (!ScannerOne.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && !ScannerTwo.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A) && ScannerThree.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
-
-    Serial.print("Scanner 3: UID: ");
-    ScannerThree.PrintHex(uidThree, uidLengthThree);
-    if((uidThree[0] == 0xB3 && uidThree[1] == 0x75 && uidThree[2] == 0x39 && uidThree[3] == 0xFA) || (uidThree[0] == 0xD3 && uidThree[1] == 0x85 && uidThree[2] == 0x03 && uidThree[3] == 0xAC)) {
-        Serial.println("Yess, you got it right this is an odd number");
-        digitalWrite(2, HIGH);  
-        delay(2000);
-        digitalWrite(2, LOW); 
- } else {
-          Serial.println("Oops, this is not an odd number try again");
-          digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-          delay(2000);                       // wait for a second
-          digitalWrite(3, LOW);
-  }
+    if (Cards[scannerIndex + 1]) {
+    Serial.println(" - This is an even number");
+    digitalWrite(GREEN_LED, HIGH);
+  } else {
+      if(!isBlank)
+        Serial.println(" - This is not an even number; try again"),
+        digitalWrite(RED_LED, HIGH);
   }
   
- // Scanner 1: UID: 0x53 0x05 0x4F 0xFA
- // Scanner 1: UID: 0x53 0x72 0xFA 0xAB
- // Scanner 2: UID: 0x33 0x26 0x84 0xFA
- // Scanner 2: UID: 0xA3 0xA5 0xE4 0xAB
- // Scanner 3: UID: 0xB3 0x75 0x39 0xFA
- // Scanner 3: UID: 0xD3 0x85 0x03 0xAC
-
-
-
-  Serial.println("----------------------------");
   delay(1000);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+}
 
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    uint8_t uid[7];
+    uint8_t uidLength;
 
-
-  
-  
+    if (scanners[i].startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A)) {
+      uint8_t success = scanners[i].readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+      if (success) {
+        handleCard(i, uid, uidLength);
+      }
+    }
+  }
 }
